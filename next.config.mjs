73e3b08e92d0cdd3f38b987bd225aed357cd0f6c
Aryan Singh <aryansingh143001@gmail.com>
2024-12-29 20:30:14 +0530

    files added

--- next.config.mjs
+++ next.config.mjs
@@ -2,13 +2,6 @@
 const isProd = process.env.NODE_ENV === 'production';
 const nextConfig = {
   reactStrictMode: true,
-  output:'export',
-  distDir:'dist',
-  images: {
-    unoptimized: true, // Disable default image optimization
-  },
-  assetPrefix: isProd ? '/divyansh-portfolio' : '',
-  basePath: isProd ? '/divyansh-portfolio' : '',
 };
 
 export default nextConfig;
